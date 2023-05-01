a8d22886317000cdf29d5fb221401c85885f4372
7te3ep <7te3ep@gmail.com>
2023-05-01 22:48:05 +0200

    🌍 geoOrthographic Projection Added Country Game

--- src/scripts/View.js
+++ src/scripts/View.js
@@ -147,28 +147,32 @@ export class CountryGame {
 
    async init() {
       await this.router.loadPage(this.link,this.path)
-      const width = 800
-      const height = 500
-
-      const projection = d3.geoMercator()
-        .translate([width / 2, height / 2])
-        .scale((width - 1) / 2 / Math.PI);
-
+      const width = window.innerWidth
+      const height = window.innerHeight
+      
+      const projection = d3.geoOrthographic()
+      .scale(200)
+      .translate([width / 2, height / 3])
+      .clipAngle(90) // sans cette option les pays de l'autre côté du globle sont visibles
+      .precision(.1)
+      .rotate([0,0,0]);
+      
       const path = d3.geoPath()
         .projection(projection);
-
+      
       const zoom = d3.zoom()
         .scaleExtent([0.2, 18])
-        .on('zoom', ()=>g.attr('transform', d3.event.transform));
-
+        .on('zoom',() => g.attr('transform', d3.event.transform))
+      
       const svg = d3.select('body').append('svg')
         .attr('width', width)
         .attr('height', height);
-
+      
       const g = svg.append('g');
-
+      
       svg.call(zoom);
-
+      
+      
       d3.json('../assets/countries.geo.json')
       .then(world => {
         g.selectAll('.country')
@@ -179,10 +183,37 @@ export class CountryGame {
           .attr('d', path)
           .style("fill", function(){return "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")"})
           .on("click",(e)=>{
-            pathClicked(e)
+              pathClicked(e)
           });
       });
-
-   d3.select("svg").attr('id',"map").on("dblclick.zoom", null); // Désactive le zoom avec un double click               
+      
+      let currentTouch = 0
+      d3.zoom().filter(() => !d3.event.button)
+      d3.select("svg").attr('id',"map").on("dblclick.zoom", null).on('mousedown.zoom',null).on('touchstart.zoom',null)
+      // Désactive le zoom avec un double click
+      
+      const λ = d3.scaleLinear()
+          .domain([0, width])
+          .range([-180, 180]);
+      
+      const φ = d3.scaleLinear()
+          .domain([0, height])
+          .range([90, -90]);
+      
+      let  drag = d3.drag().subject(function() {
+          var r = projection.rotate();
+          return {
+              x: λ.invert(r[0]),
+              y: φ.invert(r[1])
+          };
+      }).on("drag", function(event) {
+          projection.rotate([λ(d3.event.x), φ(d3.event.y)]);
+          
+          svg.selectAll(".country")
+              .attr("d", path);
+      })
+      
+      svg.call(drag);         
    }
 }
+
